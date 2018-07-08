eb218715f8db556f143a9fc0b7a6c448cbc4534c
Jett <whackashoe@gmail.com>
2018-07-08 15:16:04 -0500

    better bench print

--- bench.sh
+++ bench.sh
@@ -4,14 +4,16 @@ RUNS=1
 
 make &> /dev/null || echo "make failed"
 
+TIMEFORMAT="%E"
+
 if [ $# -eq 0 ]
 then
     for n in `find bin/ py/ -type f -not -path '*/\.*'`;
     do
-        echo "${n}:"
+        printf "%-30s" "${n}:"
         time (for i in $(seq ${RUNS}); do ./${n} > /dev/null; done)
-        echo ""
     done
 else
+    printf "%-30s" "${1}:"
     time (for i in $(seq ${RUNS}); do ./${1} > /dev/null; done)
 fi
